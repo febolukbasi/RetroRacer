71d41c86117604314ea0c20c66c117348fd01f40
febolukbasi <152182679+febolukbasi@users.noreply.github.com>
2024-03-13 12:57:17 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+let playerName = ''
+  let score = 0
+  let highScore = 0
+  let currentLane = 1
+
+  const playerNameInput = document.getElementById('playerName')
+
+  const playerNameDisplay = document.getElementById('playerNameDisplay')
+  
