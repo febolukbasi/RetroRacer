c45984cefdf1dbe1bf1f669140f83b69eee5a2f3
febolukbasi <152182679+febolukbasi@users.noreply.github.com>
2024-03-22 11:33:09 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,55 @@
-let playerName = ''
-  let score = 0
-  let highScore = 0
-  let currentLane = 1
+document.addEventListener('DOMContentLoaded', () => {
 
-  const playerNameInput = document.getElementById('playerName')
+  const player = document.getElementById('player')
+  const playButton = document.getElementById('playButton')
+  const gameScreen =document.getElementById('gameScreen')
 
-  const playerNameDisplay = document.getElementById('playerNameDisplay')
   
+  let currentLane = 1
+  let obstacles = []
+  let gameInterval
+  let obstacleSpawnInterval
+  let obstacleSpeed = 2
+  
+  
+  const lanes = [200, 250, 300]
+
+/// Audio dingen ///
+////////////////////////////////////////////////////////////////////////////////////////
+let audioSoundtrack = new Audio("audios/robot_city_8bit_soundtrack.mp3")
+// bron = https://www.youtube.com/watch?v=NAKj3HJX_tM&list=PLwJjxqYuirCLkq42mGw4XKGQlpZSfxsYd&index=21
+
+document.addEventListener('DOMContentLoaded', function() {
+  audioSoundtrack.play()
+})
+audioSoundtrack.loop = true
+
+const volumeControl = document.getElementById('volumeControl')
+
+volumeControl.addEventListener('input', function() {
+  const volume = volumeControl.value / 100
+  audioSoundtrack.volume = volume
+})
+
+pauseButton.addEventListener('click', function() {
+  if (audioSoundtrack.paused) {
+      audioSoundtrack.play()
+      pauseButton.textContent = 'Pause'
+  } else {
+      audioSoundtrack.pause()
+      pauseButton.textContent = 'Play'
+  }
+})
+
+////////////////////////////////////////////////////////////////////////////////////////
+  
+/// Game dingen ///
+////////////////////////////////////////////////////////////////////////////////////////
+
+playButton.addEventListener('click', () => {
+homeScreen.style.display = 'none'
+    gameScreen.style.display = 'flex'
+    startGame()
+})
+
+})
